bdc6e074f6e1536f93a039e3ab9e738c2c174e2f
clemjberg <clemens.jae@gmail.com>
2024-05-08 10:47:40 +0200

    Database modified

--- src/main/java/at/ac/fhcampuswien/fhmdb/repositories/WatchlistRepository.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/repositories/WatchlistRepository.java
@@ -9,5 +9,42 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class WatchlistRepository {
+    private final Dao<WatchlistMovieEntity, String> watchlistDao;
 
+    public WatchlistRepository(ConnectionSource connectionSource) throws SQLException {
+        watchlistDao = DaoManager.createDao(connectionSource, WatchlistMovieEntity.class);
+    }
+
+    public void addToWatchlist(WatchlistMovieEntity movie) throws SQLException {
+        WatchlistMovieEntity existingMovie = watchlistDao.queryBuilder()
+                .where()
+                .eq("apiId", movie.getApiId())
+                .queryForFirst();
+        if (existingMovie == null) {
+            watchlistDao.create(movie);
+        }
+    }
+
+    public void removeFromWatchlist(String apiId) throws SQLException {
+        WatchlistMovieEntity movie = watchlistDao.queryBuilder()
+                .where()
+                .eq("apiId", apiId)
+                .queryForFirst();
+        if (movie != null) {
+            watchlistDao.delete(movie);
+        }
+    }
+
+
+    public boolean isMovieInWatchlist(String apiId) throws SQLException {
+        WatchlistMovieEntity movie = watchlistDao.queryBuilder()
+                .where()
+                .eq("apiId", apiId)
+                .queryForFirst();
+        return movie != null;
+    }
+
+    public List<WatchlistMovieEntity> getWatchlist() throws SQLException {
+        return watchlistDao.queryForAll();
+    }
 }
