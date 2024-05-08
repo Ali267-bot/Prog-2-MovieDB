d81f3cb45568058c8adfe0e7a6ec2dfc5f408bd6
clemjberg <clemens.jae@gmail.com>
2024-05-08 10:46:47 +0200

    Database modified

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/WatchlistMovieEntity.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/WatchlistMovieEntity.java
@@ -3,4 +3,37 @@ package at.ac.fhcampuswien.fhmdb.models;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
+@DatabaseTable(tableName = "watchlist")
+public class WatchlistMovieEntity {
+    @DatabaseField(generatedId = true)
+    private long id;
 
+    @DatabaseField(index = true) // Optionally make it an index for faster query performance
+    private String apiId;
+
+    // ORMLite requires a no-arg constructor
+    public WatchlistMovieEntity() {
+    }
+
+    // Constructor for creating a new watchlist entry with an apiId
+    public WatchlistMovieEntity(String apiId) {
+        this.apiId = apiId;
+    }
+
+    // Getters and setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getApiId() {
+        return apiId;
+    }
+
+    public void setApiId(String apiId) {
+        this.apiId = apiId;
+    }
+}
